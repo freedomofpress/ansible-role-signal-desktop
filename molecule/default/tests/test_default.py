8dd8205b11f4671235bf5e659f49b1536b353a5c
Conor Schaefer <conor@freedom.press>
2018-10-24 16:47:30 -0700

    Let system choose gpg package name
    Some systems prefer `gnupg2` to `gpg`, so leave it up to the system
    maintainer. Apt won't work without some version of gpg, so we can assume
    it's present. The `apt-transport-https` req must be explicitly declared,
    however.
    
    Minor updates to tests for sanity-checking during local dev.
    
    N.B. you may need to edit the name of the role in
    `molecule/default/playbook.yml` for Ansible to find the path.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,14 +1,15 @@
 import os
 
+import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+@pytest.mark.parametrize('pkg', [
+    'apt-transport-https',
+    'signal-desktop',
+])
+def test_packages_installed(host, pkg):
+    assert host.package(pkg).is_installed
